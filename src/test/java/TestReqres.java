5d4a616265685d32fca75a3d728ec308d9f76bc1
DaffaPintu <daffa@pintu.co.id>
2024-10-02 08:32:48 +0700

    FIX

---
+++ src/test/java/TestReqres.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+
+public class TestReqres {
+
+    @Test
+    public void test1(){
+        int a = 10;
+        a += 5;
+        a -= 3;
+        System.out.println("Hasilnya adalah : "+a);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test Hello World 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Test Hello World 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Test Hello World 4");
+    }
+
+    @Test
+    public void test5(){
+        System.out.println("Test Hello World 5");
+    }
+}
